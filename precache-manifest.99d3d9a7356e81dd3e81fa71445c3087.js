6d56e293a34640355a30c5c9f32ae975d1adfcfd
wangyinghan <wangx6@gmail.com>
2021-03-23 08:50:40 +0000

    Updates

---
+++ precache-manifest.99d3d9a7356e81dd3e81fa71445c3087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63c81990c63d94675cb9347f0552fd9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c01d51c6d20a7e71420",
+    "url": "./static/css/main.5d4026af.chunk.css"
+  },
+  {
+    "revision": "0926592a70a4dbdbf427",
+    "url": "./static/js/2.3760e7d5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3760e7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c01d51c6d20a7e71420",
+    "url": "./static/js/main.596a9d07.chunk.js"
+  },
+  {
+    "revision": "281c3f4ee9952cd0a198",
+    "url": "./static/js/runtime-main.d6b241e9.js"
+  },
+  {
+    "revision": "843b73f200a87d95d96a980ca09dc073",
+    "url": "./static/media/logo.843b73f2.png"
+  }
+]);
